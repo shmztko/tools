0f521c80e149970bf6bf61c1548ea52862702487
takewo <st0098@gmail.com>
2012-11-09 13:14:43 +0900

    JVMの各種情報を取得するシェル作成

---
+++ get_jvm_dump.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+echo 'JVM dump start.'
+echo `date`
+
+now=`date +%Y%m%d%H%M%S`
+current_dir=`pwd`
+
+# TomcatのプロセスIDを取得
+java_proc_id=`jps | grep Bootstrap | awk -F" " '{ print $1 }'`
+if [ "$java_proc_id" = "" ]
+then
+  echo 'No java process to dump.'
+  exit 1
+fi
+echo "Java Process ID -> $java_proc_id"
+
+# ヒープダンプを出力
+echo "Java heap dump start."
+jmap -dump:format=b,file=jmap-$now.bin $java_proc_id
+echo "Java heap dump finish. -> $current_dir/jmap-$now.bin created."
+
+# スレッドダンプを出力
+echo "Java thread dump start."
+jstack $java_proc_id > jstack-$now.log
+echo "Java thread dump finish. -> $current_dir/jstack-$now.log created."
